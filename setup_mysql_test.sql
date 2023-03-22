be65a2d6b3f2e9e51b28751fe57bec822d92864d
myrajarenga <myrajarenga1234@gmail.com>
2023-03-22 07:00:02 -0700

    setting up mysql test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,7 @@
 -- Creates A DataBase and a Test User
+-- a new user hbnb_test in localhost
+-- grants all privileges to the user on the hbnb_test_db database
+-- grants SELECT privelege on the performance_schema database
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
