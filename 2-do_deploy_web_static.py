830b7f307b3e1aae9509ef224f9d0b56f590003f
myrajarenga <myrajarenga1234@gmail.com>
2023-04-06 08:45:00 -0700

    deploying archives

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" sending an archive file to a remote server
+and decompressing it"""
+
+from fabric.api import run, env, put
+import os.path
+
+env.hosts = ['34.224.94.210', '54.87.252.193']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+    """a function to deploy code and decompress it"""
+    
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+    
+    try:
+       remote_path = "/data/web_static/releases/{}/".format(no_extension)
+       sym_link = "/data/web_static/current"
+       put(archive_path, "/tmp/")
+       run("sudo mkdir - p {}".format(remote_path))
+       run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+       run("sudo rm /tmp/{}".format(compressed_file))
+       run("sudo ln -sf {} {}".format(remote_path, sym_link))
+       return True
+    except Exception as e:
+       return False
