c738a635356004b14aca2a133b2cdac6ee5ba512
myrajarenga <myrajarenga1234@gmail.com>
2023-04-07 00:37:55 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,11 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
-"""send an archive file to a remote server
-and decompress it"""
-=======
+#!/usr/bin/python3=
 """This Script sends A File to specified Remote Servers"""
-
->>>>>>> 7e869e2cbb686efd8e5c5caf7d008e93ea3e4340
 from fabric.api import run, env, put
 import os.path
 
 """ Environment Variables """
 
-env.hosts = ['34.227.101.223', '18.204.15.60']
+env.hosts = ['34.224.94.210', '54.87.252.193']
 env.key_filename = '~/.ssh/school'
 env.user = 'ubuntu'
 
@@ -28,7 +22,6 @@ def do_deploy(archive_path):
     no_ext = tgz_file.split(".")[0]
 
     try:
-<<<<<<< HEAD
        remote_path = "/data/web_static/releases/{}/".format(no_extension)
        sym_link = "/data/web_static/current"
        put(archive_path, "/tmp/")
@@ -42,39 +35,3 @@ def do_deploy(archive_path):
        return True
     except Exception as e:
        return False
-=======
-        """ Servers Remote Path variable"""
-
-        remote_path = "/data/web_static/releases/{}/".format(no_ext)
-        """ Symbolic Link """
-
-        sym_link = "/data/web_static/current"
-        """Put archive path in servers /tmp/"""
-
-        put(archive_path, "/tmp/")
-
-        """Makes Remote Path Direcory in the server """
-
-        run("sudo mkdir -p {}".format(remote_path))
-        """Decompresses the tgz_file to the Remote path"""
-
-        run("sudo tar -xvzf /tmp/{} -C {}".format(tgz_file, remote_path))
-        """Remove the compressed file in /tmp/ direcory"""
-
-        run("sudo rm /tmp/{}".format(tgz_file))
-        """Move Everything in web_static to the releases"""
-
-        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
-        """Remove Web_static"""
-
-        run("sudo rm -rf {}/web_static".format(remote_path))
-        """Remove the /data/web_static/current link"""
-
-        run("sudo rm -rf /data/web_static/current")
-        """Create A New current Link"""
-
-        run("sudo ln -sf {} {}".format(remote_path, sym_link))
-        return True
-    except Exception as f:
-        return False
->>>>>>> 7e869e2cbb686efd8e5c5caf7d008e93ea3e4340
