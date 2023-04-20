0d9c28f51d15cb41e3e1989f09843e91c7e2816a
JosiahMbithi001 <mbithishucks@gmail.com>
2023-04-20 09:26:35 -0700

    3-Python_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,17 +6,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-	"""A Method to return Hello HBNB!"""
-	
-	return "Hello HBNB!"
+    """A Method to return Hello HBNB!"""
+
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""A method to return HBNB!"""
-	
-	return "HBNB"
+    """A method to return HBNB!"""
+
+    return "HBNB"
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This Script Returns C followe by A text to be put in the Path """
+"""This Script Returns Python followe by A text to be put in the Path """
 
 
 from flask import Flask
@@ -26,6 +26,19 @@ def c(text):
     """A Method that returns C and text"""
 
     return f'C {text.replace("_"," ")}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text=None):
+    """
+    A Python Method that displays Python is Cool by Default
+    else returns Python +  passed in text
+    """
+    if text:
+        return "Python " + text.replace('_', ' ')
+    else:
+        return "Python is cool"
 
 
 if __name__ == "__main__":
