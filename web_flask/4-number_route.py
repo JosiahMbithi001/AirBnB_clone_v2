20bb84f9c3e216b951b11cc11ce15eb120df3d1e
JosiahMbithi001 <mbithishucks@gmail.com>
2023-04-20 10:46:18 -0700

    4-number_route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This Script Returns A Confirmation that input n is a Number """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """A Method to return Hello HBNB!"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A method to return HBNB!"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """A Method that returns C and text"""
+
+    return f'C {text.replace("_"," ")}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text=None):
+    """
+    A Python Method that displays Python is Cool by Default
+    else returns Python +  passed in text
+    """
+    if text:
+        return "Python " + text.replace('_', ' ')
+    else:
+        return "Python is cool"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+	"""A Method that returns n if n is an int"""
+	return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
