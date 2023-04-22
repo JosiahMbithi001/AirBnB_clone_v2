84c57284253d9ba026c989ac18d9eaedd6cd2ed7
JosiahMbithi001 <mbithishucks@gmail.com>
2023-04-22 06:22:28 -0700

    pycode fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,16 +8,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-	"""Renders State Template"""
-	state=storage.all(State)
-	return render_template("7-states_list.html",state=state)
+    """Renders State Template"""
+    state = storage.all(State)
+    return render_template("7-states_list.html", state=state)
+
 
 @app.teardown_appcontext
 def clean(exception=None):
-	"""Tearsdown Connection"""
-	storage.close()
+    """Tearsdown Connection"""
+    storage.close()
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
